f1f3a0fc1c10a5cac222b5a886d30c288ac7144b
tonytrejo <you@example.com>
2018-04-21 14:28:52 -0700

    user routes fixed passwords are hashed

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
-//const gravatar = require("gravatar");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
@@ -21,28 +20,21 @@ router.get("/test", (req, res) =>
 // desc - Register user
 // access - Public
 router.post("/register", (req, res) => {
+//res.json({body: req.body});
   // use mongoose to find email if exist
-  User.findOne({ email: req.body.email })
-    //promise
-    .then(user => {
-      if (user) {
-        return res.status(400).json({ email: "Email Already Registered" });
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: "200", // Size
-          r: "pg", // Rating
-          d: "mm" // Default
-        });
-
-        console.log(req.body);
+   User.findOne({ email: req.body.email })
+     //promise
+     .then(user => {
+       if (user) 
+         return res.status(400).json({ email: "Email Already Registered" });
+     console.log(req.body);
         bcrypt.genSalt(10, (err, salt) => {
-          console.log(salt);
+         // console.log(salt);
           bcrypt.hash(req.body.password, salt, (err, hash) => {
             if (err) return res.json(err);
             const newUser = new User({
               name: req.body.name,
               email: req.body.email,
-              avatar: avatar,
               password: hash
             });
             newUser
@@ -50,10 +42,9 @@ router.post("/register", (req, res) => {
               .then(user => res.json(user))
               .catch(err => console.log(err));
           });
-        });
-      }
-    });
-});
+      });
+       });
+      });      
 
 // route - get request  api/user/login
 // desc - Login User / Returning JWT Token
@@ -67,15 +58,15 @@ router.post("/login", (req, res) => {
     if (!user) {
       return res.status(404).json({ email: "User Not Valid" });
     }
-    // Check Password
-    bcrypt.compare(password, user.password).then(matching => {
-      if (matching) {
-        //User Matched
+   // Check Password
+   bcrypt.compare(password, user.password).then(matching => {
+     if (matching) {
+       //User Matched
         //res.json({ msg: "Varified" });
         const token = {
           id: user.id,
           name: user.name,
-          avatar: user.avatar
+      //    avatar: user.avatar
         }; //Create JWT Token
         //Sign Token
         jwt.sign(
@@ -109,4 +100,3 @@ router.get(
 
 // Export Router
 module.exports = router;
-
